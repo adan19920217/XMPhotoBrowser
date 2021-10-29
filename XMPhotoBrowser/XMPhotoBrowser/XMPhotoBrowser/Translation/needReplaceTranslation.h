f645ac4b21d680406c4b2d68e38a6078bb173d1c
weipengcheng <15510705793@163.com>
2021-10-29 13:10:05 +0800

    needReplaceFiles

---
+++ XMPhotoBrowser/XMPhotoBrowser/XMPhotoBrowser/Translation/needReplaceTranslation.h
@@ -0,0 +1,16 @@
+//
+//  needReplaceTranslation.h
+//  XMPhotoBrowser
+//
+//  Created by weipengcheng on 2021/10/29.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface needReplaceTranslation : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ XMPhotoBrowser/XMPhotoBrowser/XMPhotoBrowser/Translation/needReplaceTranslation.m
@@ -0,0 +1,12 @@
+//
+//  needReplaceTranslation.m
+//  XMPhotoBrowser
+//
+//  Created by weipengcheng on 2021/10/29.
+//
+
+#import "needReplaceTranslation.h"
+
+@implementation needReplaceTranslation
+
+@end
